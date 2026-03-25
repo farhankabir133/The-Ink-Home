8d0b730e3984688e57075862535815d46baccad8
Farhan Kabir <autosparkbd@gmail.com>
2026-03-26 00:51:25 +0600

    fix(icons): replace external-link glyphs with right-arrow chevrons on CTA buttons

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -237,7 +237,7 @@ const HomePage: React.FC = () => {
                 >
                   Read on Medium
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 12h14M12 5l7 7-7 7" />
                   </svg>
                 </a>
               </div>
@@ -562,7 +562,7 @@ const HomePage: React.FC = () => {
           >
             Follow on Medium
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 12h14M12 5l7 7-7 7" />
             </svg>
           </a>
           <Link
